043320ad9176b4ebc84dad97a96d223765c0e58b
Pethum-Jeewantha <pethumjeewantha1@gmail.com>
2021-06-18 20:31:43 +0530

    implement mongodb & create model

---
+++ item-api/index.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const mongoose = require('mongoose')
+
+const app = express();
+
+mongoose.connect(
+    'mongodb://localhost:27017/bloomingdales',
+    {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+        useFindAndModify: false,
+        useCreateIndex: true
+    }
+).then(()=>{
+    app.listen(3000,()=>{
+        console.log('Item service up & running on port 3000');
+    })
+}).catch(error=>{
+    console.log(error)
+});
---
+++ item-api/model/ItemSchema.js
@@ -0,0 +1,24 @@
+const mongoose = require('mongoose');
+
+const ItemSchema = new mongoose.Schema({
+    itemName:{
+        type: String,
+        required:true
+    },
+    itemSize:{
+        type: Number,
+        required:true
+    },
+    itemColor:{
+        type: String,
+        required:true
+    },
+    itemQty:{
+        type: Number,
+        required:true
+    },
+    itemPrice:{
+        type:Number,
+        required:true
+    }
+});
