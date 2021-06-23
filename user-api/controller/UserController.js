96ca1f498940cdb4d2291fd2dd3c643cacb01e42
Pethum-Jeewantha <pethumjeewantha1@gmail.com>
2021-06-23 17:19:23 +0530

    Finished save users

--- user-api/controller/UserController.js
+++ user-api/controller/UserController.js
@@ -4,6 +4,15 @@ const bcrypt = require('bcryptjs');
 const saveUser =async (req, resp) => {
 
     const {userEmail, userPassword : plainTextPassword} = req.body;
+
+    if(typeof  userEmail !== 'string'){
+        return resp.json({status: 'error', error: 'Invalid Email'})
+    }
+    if(typeof  plainTextPassword !== 'string'){
+        return resp.json({status: 'error', error: 'Invalid Password'})
+    }
+
+
     const userPassword  = await bcrypt.hash(plainTextPassword, 10)
 
 
@@ -14,8 +23,12 @@ const saveUser =async (req, resp) => {
         });
         console.log('user Created', res)
     } catch (error) {
-        console.log(error);
-        return resp.json({status: 'error'});
+//        console.log(error);
+
+        if (error.code === 11000) {
+            return resp.json({status: 'error', error: 'Email already in use'});
+        }
+        throw error;
     }
 
 
