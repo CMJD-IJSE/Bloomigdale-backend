6cb21c3b3e44ea7668512e56398c15353f2c8170
Pethum-Jeewantha <pethumjeewantha1@gmail.com>
2021-06-23 14:18:10 +0530

    Added saveUser login

--- user-api/controller/UserController.js
+++ user-api/controller/UserController.js
@@ -1,16 +1,40 @@
 const User = require('../model/UserSchema');
+const bcrypt = require('bcryptjs');
 
-const saveUser =(req,resp)=>{
-    const user = new User({
+const saveUser =async (req, resp) => {
+    /*const user = new User({
         userEmail:req.body.userEmail,
         userPassword:req.body.userPassword
     });
-    console.log(user)
+    console.log(user);*/
 
-    user.save().then(result=>{
-        resp.status(200).json({state:true,"message":"saved"});
-    }).catch(error=>{
-        resp.status(500).json(error)
+    /*const {userEmail, userPassword: plainTextPassword} = req.body;
+
+    console.log(req.body)
+
+    const userPassword = await bcrypt.hash(plainTextPassword, 10)
+
+    try {
+        const res = await User.create({
+            userEmail,
+            userPassword
+        });
+        console.log('user Created', res)
+    } catch (error) {
+        console.log(error);
+        return res.json({status: 'error'});
+    }*/
+
+
+    /*    user.save().then(result=>{
+            console.log(user);
+            resp.status(200).json({state:true,"message":"saved"});
+        }).catch(error=>{
+            resp.status(500).json(error)
+        })*/
+    
+    User.pre("save",async function f() {
+        
     })
 }
 
--- user-api/model/UserSchema.js
+++ user-api/model/UserSchema.js
@@ -1,15 +1,18 @@
 const mongoose = require('mongoose');
 
 const UserSchema = new mongoose.Schema({
-    userEmail:{
-        type: String,
-        required: true,
-        unique: true
+        userEmail: {
+            type: String,
+            required: true,
+            unique: true
+        },
+        userPassword: {
+            type: String,
+            required: true
+        }
     },
-    userPassword:{
-        type: String,
-        required: true
-    }
-});
+    {collection: 'Users'}
+);
 
-module.exports=mongoose.model('User', UserSchema);
+
+module.exports = mongoose.model('User', UserSchema);
